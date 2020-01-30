9682b581e5aa765c0672f6239b093f33a04a8583
Andrew Hyder <andrew.hyder@gsa.gov>
2020-01-30 10:57:43 -0800

    Prints out list of XML sorns.

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 We envision a future in which the public can easily understand how and why personally identifiable information gets collected by government agencies. 
 
 This project is funded by [10x](https://10x.gsa.gov/).
+
+
+## Installation
+```
+virtualenv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
---
+++ sorn_scraper.py
@@ -0,0 +1,52 @@
+import sys, time
+import requests
+from bs4 import BeautifulSoup
+
+class AgencySorns:
+  def __init__(self, url):
+    '''
+    Give it a url of a agency's list of SORNs.
+    '''
+    self.url = url
+    self.sorns = []
+
+  def get_sorns(self):
+    result = requests.get(self.url)
+    soup = BeautifulSoup(result.text, 'html.parser')
+    for link in soup.find_all('a'): # It helps to find all anchor tag's
+        if "https://www.federalregister.gov" in link.get('href'):
+          sorn = Sorn(link.get('href'))
+          self.sorns.append(sorn)
+
+
+class Sorn:
+  def __init__(self, html_url):
+    self.html_url = html_url
+    self.xml_url = self.build_xml_url()
+    
+  def build_xml_url(self):
+    '''
+    transform the html url to the xml url
+
+    Example html and xml urls
+    https://www.federalregister.gov/documents/2015/06/04/2015-13701/privacy-act-of-1974-notice-of-an-updated-system-of-records
+    https://www.federalregister.gov/documents/full_text/xml/2015/06/04/2015-13701.xml
+    '''
+    split_url = self.html_url.split("/")
+    first_half = "/".join(split_url[0:4])
+    second_half = "/".join(split_url[4:8])
+    return first_half + "/full_text/xml/" + second_half + ".xml"
+
+
+  # def validate_all_xml_urls(self):
+  #   for xml_url in self.sorns_xml_urls:
+  #     response = requests.get(xml_url)
+  #     time.sleep(0.1)
+  #     if response.status_code != 200:
+  #       print("Ruh roh: " + xml_url)
+
+if __name__ == '__main__':
+  agency_sorns = AgencySorns(sys.argv[1])
+  agency_sorns.get_sorns()
+  for sorn in agency_sorns.sorns:
+    print(sorn.xml_url)
