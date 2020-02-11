d8af1580c64ba107fd60031f2c82969cf33d7532
Andrew Hyder <andrew.hyder@gsa.gov>
2020-02-11 10:38:34 -0800

    NLP experiments.
    Three approaches to getting a list of categories of records from paragraph data.

--- noun_finder.py
+++ noun_finder.py
@@ -3,42 +3,53 @@ import nltk
 from textblob import TextBlob
 
 
-# with open('gsa_sorns.csv') as sorn_csv:
-#   reader = csv.DictReader(sorn_csv)
-#   for row in reader:
-#     blob = TextBlob(row['PII'])
-#     print(blob.noun_phrases)
+# TextBlob approach
+with open('gsa_sorns.csv') as sorn_csv:
+  with open('noun_extracting/TextBlob.csv', 'w') as output:
+    reader = csv.DictReader(sorn_csv)
+    writer = csv.writer(output)
+    for row in reader:
+      blob = TextBlob(row['PII'])
+      system_name_and_noun_phrases = [row['System Name']] + blob.noun_phrases
+      writer.writerow(system_name_and_noun_phrases)
 
 
-# # function to test if something is a noun
-# is_noun = lambda pos: pos[:2] == 'NN'
-# # do the nlp stuff
-# with open('gsa_sorns.csv') as sorn_csv:
-#   reader = csv.DictReader(sorn_csv)
-#   for row in reader:
-#     tokenized = nltk.word_tokenize(row['PII'])
-#     nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)] 
-#     print(nouns)
+# Simple nlp noun checker
+is_noun = lambda pos: pos[:2] == 'NN'
+with open('gsa_sorns.csv') as sorn_csv:
+  with open('noun_extracting/simple_nlp_nouns.csv', 'w') as output:
+    reader = csv.DictReader(sorn_csv)
+    writer = csv.writer(output)
+    for row in reader:
+      tokenized = nltk.word_tokenize(row['PII'])
+      nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)] 
+      system_name_and_nouns = [row['System Name']] + nouns
+      writer.writerow(system_name_and_nouns)
 
 
-
-# grammar = "NP: {<JJ>*<NN>+<JJ>*<NN>*}"
-# cp = nltk.RegexpParser(grammar)
-
-# # do the nlp stuff
-# with open('gsa_sorns.csv') as sorn_csv:
-#   reader = csv.DictReader(sorn_csv)
-#   for row in reader:
-#     sentences = nltk.sent_tokenize(row['PII'])
-#     sentences = [nltk.word_tokenize(sent) for sent in sentences]
-#     sentences = [nltk.pos_tag(sent) for sent in sentences]
-#     for sentence in sentences:
-#       result = cp.parse(sentence)
-#       for chunk in result:
-#         try:
-#           print(" ".join([word for (word, tag) in chunk.leaves()]))
-#         except:
-#           pass
+# More complicated nlp phrase checker 
+grammar = "NP: {<JJ>*<NN>+<JJ>*<NN>*}"
+cp = nltk.RegexpParser(grammar)
+with open('gsa_sorns.csv') as sorn_csv:
+  with open('noun_extracting/complicated_nlp_phrases.csv', 'w') as output:
+    reader = csv.DictReader(sorn_csv)
+    writer = csv.writer(output)
+    for row in reader:
+      sentences = nltk.sent_tokenize(row['PII'])
+      sentences = [nltk.word_tokenize(sent) for sent in sentences]
+      sentences = [nltk.pos_tag(sent) for sent in sentences]
+      phrases = []
+      for sentence in sentences:
+        result = cp.parse(sentence)
+        for chunk in result:
+          try:
+            phrase = [word for (word, tag) in chunk.leaves()]
+            phrase = " ".join(phrase)
+            phrases.append(phrase)
+          except:
+            pass
+      system_name_and_phrases = [row['System Name']] + phrases
+      writer.writerow(system_name_and_phrases)
 
 
     
